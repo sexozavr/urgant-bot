82b3adb22f6ed26a7198b75007cf7d0c3b0319a9
sexozavr <daniildiveev@yandex.ru>
2021-12-25 02:11:05 +0300

    Initial commit

---
+++ parse.py
@@ -0,0 +1,24 @@
+import threading
+from parsers import *
+
+parsers = (tatler_parser, sobaka_parser, esquire_parser, kommersant_parser, rbc_parser, 
+            dp_parsing, forbes_parser, sports_ru_parser, village_parser, flow_parser, elle_parser, 
+            glamour_parser,)
+
+def parse(query:str) -> list:
+    global parsers
+
+    thread_list, results = [], []
+
+    for func in parsers:
+        thread = threading.Thread(target=func, args=(query,results))
+        thread_list.append(thread,)
+        thread.start()
+    
+    for i in range(len(thread_list)):
+        thread_list[i].join()
+
+    return results
+
+texts = parse(input('Please choose a celebrity: '))
+print(texts)
